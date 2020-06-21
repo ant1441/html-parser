a07ab654a492be8412cd09d4bac34258d94be4e6
adam <ant1441@gmail.com>
2020-06-21 16:57:22 +0100

    Dom: Implement `Deref` and `DerefMut` for Text

--- src/dom/text.rs
+++ src/dom/text.rs
@@ -1,13 +1,7 @@
-use derive_more::{Constructor, From};
+use derive_more::{Constructor, From, Deref, DerefMut};
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Constructor, Debug, Default, Deserialize, Eq, From, Hash, PartialEq, Serialize)]
+#[derive(Clone, Constructor, Debug, Default, Deserialize, Eq, From, Hash, PartialEq, Serialize, Deref, DerefMut)]
 pub struct Text {
     data: String,
-}
-
-impl Text {
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
 }
