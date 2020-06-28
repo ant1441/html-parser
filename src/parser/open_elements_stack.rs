7679fc8111100767b6cf626bbc2d6ea446432b88
adam <ant1441@gmail.com>
2020-06-28 16:37:41 +0100

    Parser: `OpenElementsStack::contains_any_elements` & `OpenElementsStack::pop_until`

--- src/parser/open_elements_stack.rs
+++ src/parser/open_elements_stack.rs
@@ -1,6 +1,7 @@
 use std::{cell::RefCell, rc::Rc};
 
 use derive_more::{Deref, DerefMut};
+use log::trace;
 
 use crate::{dom, tokenizer::TagName};
 
@@ -14,12 +15,29 @@ impl OpenElementsStack {
         OpenElementsStack { stack: Vec::new() }
     }
 
+    /// Returns true if any of the elements in the stack have the given `name`
+    #[must_use]
     pub(crate) fn contains_element(&self, name: &TagName) -> bool {
+        self.contains_any_elements(&[name])
+    }
+
+    /// Returns true if any of the elements in the stack have any of the given `names`
+    #[must_use]
+    pub(crate) fn contains_any_elements(&self, names: &[&TagName]) -> bool {
         self.stack
             .iter()
             .by_ref()
-            .filter(|e| e.borrow().name() == name)
-            .count()
-            > 0
+            .any(|e| names.iter().any(|name| &e.borrow().name() == name))
+    }
+
+    /// Pop elements off the stack until one of `names` has been popped
+    pub(crate) fn pop_until(&mut self, names: &[&TagName]) {
+        while let Some(e) = self.pop() {
+            let elem = e.borrow();
+            if names.iter().any(|name| &&elem.name == name) {
+                break;
+            }
+            trace!("InBody: Popped {:?} off stack", elem);
+        }
     }
 }
